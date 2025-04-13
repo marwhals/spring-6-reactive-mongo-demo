6847db1f061568c55c9385c2bc2de1ff39d41c5f
marwhals <marjan.mubarok2012@gmail.com>
2025-04-13 13:22:41 +0100

    Setup project and dependencies

---
+++ src/main/java/spring/example/reactive_mongo_demo/ReactiveMongoDemoApplication.java
@@ -0,0 +1,13 @@
+package spring.example.reactive_mongo_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMongoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveMongoDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/example/reactive_mongo_demo/ReactiveMongoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.example.reactive_mongo_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveMongoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
