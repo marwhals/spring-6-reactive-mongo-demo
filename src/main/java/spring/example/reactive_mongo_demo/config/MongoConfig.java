233962a7cbd4d236644703a7ba7ab62bf8dfcec1
marwhals <marjan.mubarok2012@gmail.com>
2025-04-18 15:02:11 +0100

    Setup remote mongo

--- src/main/java/spring/example/reactive_mongo_demo/config/MongoConfig.java
+++ src/main/java/spring/example/reactive_mongo_demo/config/MongoConfig.java
@@ -1,10 +1,15 @@
 package spring.example.reactive_mongo_demo.config;
 
-import com.mongodb.reactivestreams.client.MongoClient; //Ensure this is correct package
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+
+import java.util.Collections;
 
 @Configuration
 public class MongoConfig extends AbstractReactiveMongoConfiguration {
@@ -17,5 +22,17 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration {
     @Override
     protected String getDatabaseName() {
         return "local";
+    }
+
+    @Override
+    protected void configureClientSettings(MongoClientSettings.Builder builder) {
+        builder.credential(MongoCredential.createCredential(
+                        "root",
+                        "admin",
+                        "example".toCharArray()))
+                .applyToClusterSettings(
+                        settings -> {
+                            settings.hosts((Collections.singletonList(new ServerAddress("192.169.1.105", 27017))));
+                        });
     }
 }
